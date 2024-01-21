02739014531f20e23181b3234b3bae3087e9b58b
Bogdan Tatarov <bogdan@tatarov.me>
2024-01-22 04:40:05 +0800

    renderer: apply color to the fragment shader

--- src/renderer/shader/glsl/fragment.glsl
+++ src/renderer/shader/glsl/fragment.glsl
@@ -1,6 +1,6 @@
 #version 330 core
 
-in vec3 vertex_color;
+in vec4 vertex_color;
 in vec2 uv;
 
 uniform sampler2D u_texture;
@@ -8,5 +8,5 @@ uniform sampler2D u_texture;
 out vec4 fragment_color;
 
 void main() {
-    fragment_color = texture(u_texture, uv);
+    fragment_color = texture(u_texture, uv) * vertex_color;
 }
--- src/renderer/shader/glsl/vertex.glsl
+++ src/renderer/shader/glsl/vertex.glsl
@@ -1,12 +1,12 @@
 #version 330 core
 
 layout (location = 0) in vec3 a_position;
-layout (location = 1) in vec3 a_color;
+layout (location = 1) in vec4 a_color;
 layout (location = 2) in vec2 a_uv;
 
 uniform mat4 u_projection;
 
-out vec3 vertex_color;
+out vec4 vertex_color;
 out vec2 uv;
 
 void main() {
