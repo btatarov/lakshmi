b4fdc51f6b359846d27764e5b52ec432f27a7908
Bogdan Tatarov <bogdan@tatarov.me>
2024-09-12 20:32:11 +0800

    box2d: test code improvements

--- test/box2d/main.lua
+++ test/box2d/main.lua
@@ -14,16 +14,16 @@ LakshmiRenderer.add(layer)
 
 LakshmiBox2DWorld.init()
 LakshmiBox2DWorld.setUnitsPerMeter(100)
-LakshmiBox2DWorld.setGravity(0, 9.80665)
+LakshmiBox2DWorld.setGravity(0, -9.80665)
 LakshmiBox2DWorld.setUpdateSteps(8)
 
 floor = LakshmiBox2DBox.new(700, 35)
 floor:setBodyType(LakshmiBox2DWorld.BODY_TYPE_STATIC)
-floor:setPos(0 + 700 - 200, 768 - 35)
+floor:setPos(0, - 768 / 2 + 35)
 
 x, y = floor:getPos()
 floor_sprite = LakshmiSprite.new('test/box2d/floor.png')
-floor_sprite:setPos(- 1024 / 2 + x, 768 / 2 - y)
+floor_sprite:setPos(x, y)
 layer:add(floor_sprite)
 
 boxes = {}
@@ -31,16 +31,14 @@ for i = 1, 15 do
     for j = 1, 12 do
         local box = LakshmiBox2DBox.new(15, 15)
         box:setBodyType(LakshmiBox2DWorld.BODY_TYPE_DYNAMIC)
-        box:setPos(50 + 65 * (i - 1), 50 + 55 * (j - 1))
+        box:setPos(- 1024 / 2 + 50 + 65 * (i - 1), 768 / 2 - (50 + 55 * (j - 1)))
         box:setRot(math.random() * 360)
         box:setFriction(0.6)
         box:setRestitution(0.5)
 
-        local x, y = box:getPos()
-        local r = box:getRot()
         local sprite = LakshmiSprite.new('test/box2d/crate.png')
-        sprite:setPos(- 1024 / 2 + x, 768 / 2 - y)
-        sprite:setRot(r)
+        sprite:setPos(box:getPos())
+        sprite:setRot(box:getRot())
         layer:add(sprite)
 
         table.insert(boxes, {box = box, sprite = sprite})
@@ -50,9 +48,8 @@ end
 LakshmiWindow.setLoopCallback(function(delta)
     LakshmiBox2DWorld.update(delta)
 
-    for _, v in ipairs(boxes) do
-        local x, y = v.box:getPos()
-        v.sprite:setPos(- 1024 / 2 + x, 768 / 2 - y)
-        v.sprite:setRot(v.box:getRot())
+    for _, box in ipairs(boxes) do
+        box.sprite:setPos(box.box:getPos())
+        box.sprite:setRot(box.box:getRot())
     end
 end)
