1f26c41a9f05465da1eb083b2b7f73bcd081ef1c
Bogdan Tatarov <bogdan@tatarov.me>
2024-09-12 20:01:29 +0800

    box2d: better test with graphics

--- test/box2d/main.lua
+++ test/box2d/main.lua
@@ -9,22 +9,50 @@ LakshmiKeyboard.setCallback(function(key, action)
     end
 end)
 
+layer = LakshmiLayer.new()
+LakshmiRenderer.add(layer)
+
 LakshmiBox2DWorld.init()
-LakshmiBox2DWorld.setUnitsPerMeter(10)
+LakshmiBox2DWorld.setUnitsPerMeter(100)
 LakshmiBox2DWorld.setGravity(0, 9.80665)
 LakshmiBox2DWorld.setUpdateSteps(8)
 
-box = LakshmiBox2DBox.new(1024 / 2, 768 / 2)
-box:setBodyType(LakshmiBox2DWorld.BODY_TYPE_DYNAMIC)
-box:setPos(100, 100)
-box:setRot(35)
-box:setFriction(0.6)
-box:setRestitution(0.3)
+floor = LakshmiBox2DBox.new(700, 35)
+floor:setBodyType(LakshmiBox2DWorld.BODY_TYPE_STATIC)
+floor:setPos(0 + 700 - 200, 768 - 35)
+
+x, y = floor:getPos()
+floor_sprite = LakshmiSprite.new('test/box2d/floor.png')
+floor_sprite:setPos(- 1024 / 2 + x, 768 / 2 - y)
+layer:add(floor_sprite)
+
+boxes = {}
+for i = 1, 8 do
+    for j = 1, 4 do
+        local box = LakshmiBox2DBox.new(35, 35)
+        box:setBodyType(LakshmiBox2DWorld.BODY_TYPE_DYNAMIC)
+        box:setPos(60 + 120 * (i - 1), 80 + 120 * (j - 1))
+        box:setRot(math.random() * 360)
+        box:setFriction(0.6)
+        box:setRestitution(0.5)
+
+        local x, y = box:getPos()
+        local r = box:getRot()
+        local sprite = LakshmiSprite.new('test/box2d/crate.png')
+        sprite:setPos(- 1024 / 2 + x, 768 / 2 - y)
+        sprite:setRot(r)
+        layer:add(sprite)
+
+        table.insert(boxes, {box = box, sprite = sprite})
+    end
+end
 
 LakshmiWindow.setLoopCallback(function(delta)
     LakshmiBox2DWorld.update(delta)
-    print('box pos:', box:getPos())
-    print('box rot:', box:getRot())
-    print('box friction:', box:getFriction())
-    print('box restitution:', box:getRestitution())
+
+    for _, v in ipairs(boxes) do
+        local x, y = v.box:getPos()
+        v.sprite:setPos(- 1024 / 2 + x, 768 / 2 - y)
+        v.sprite:setRot(v.box:getRot())
+    end
 end)
