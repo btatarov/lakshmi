30d4525d32190009c825a3de9672ef43c18e65fb
Bogdan Tatarov <bogdan@tatarov.me>
2024-09-16 17:51:15 +0800

    project: better build script

--- build.sh
+++ build.sh
@@ -1,2 +1,20 @@
 #!/bin/sh
-odin run src/ -out=build/lakshmi -vet -debug -show-timings -- test/main.lua
+DEFAULT_BUILD="debug"
+ALLOWED_BUILDS=("release" "debug")
+DEFAULT_SCRIPT="test/main.lua"
+
+build=${1:-$DEFAULT_BUILD}
+script=${2:-$DEFAULT_SCRIPT}
+
+if [[ ! " ${ALLOWED_BUILDS[@]} " =~ " ${build} " ]]; then
+    echo "Invalid build type: $build"
+    echo "Valid values are: ${ALLOWED_BUILDS[@]}"
+    exit 1
+fi
+
+extra_args="-show-timings"
+if [ $build == "debug" ]; then
+    extra_args="$extra_args -debug -vet"
+fi
+
+odin run src/ -out=build/lakshmi $extra_args -- $script
