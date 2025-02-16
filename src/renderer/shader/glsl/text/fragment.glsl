fe23e9736ee679f6b568dc42ba976ea761986052
Bogdan Tatarov <bogdan@tatarov.me>
2025-02-16 17:00:35 +0800

    text: update shader to support opacity

--- src/renderer/shader/glsl/text/fragment.glsl
+++ src/renderer/shader/glsl/text/fragment.glsl
@@ -8,5 +8,9 @@ uniform sampler2D u_texture;
 out vec4 fragment_color;
 
 void main() {
-    fragment_color = vec4(vertex_color.xyz, texture(u_texture, uv).r);
+    fragment_color = vec4(vertex_color.rgb, texture(u_texture, uv).r);
+    fragment_color.r = mix(0, 1, fragment_color.r * vertex_color.a);
+    fragment_color.g = mix(0, 1, fragment_color.g * vertex_color.a);
+    fragment_color.b = mix(0, 1, fragment_color.b * vertex_color.a);
+    fragment_color.a = mix(0, 1, fragment_color.a * vertex_color.a);
 }
